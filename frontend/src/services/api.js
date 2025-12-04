2bd3c0b1456fda4a25a84a2a3c6bb4b848d1bba5
yohandry12 <belingagerald@gmail.com>
2025-12-04 12:37:11 +0100

    api.js update

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -32,9 +32,9 @@ apiClient.interceptors.response.use(
   },
   (error) => {
     // --- AJOUTEZ CECI POUR VOIR L'ERREUR SUR LE TÉLÉPHONE ---
-    const errorMessage = error.response?.data?.error || error.message;
-    const url = error.config?.url;
-    alert(`ERREUR API : ${url}\nMessage : ${errorMessage}`);
+    // const errorMessage = error.response?.data?.error || error.message;
+    // const url = error.config?.url;
+    // alert(`ERREUR API : ${url}\nMessage : ${errorMessage}`);
     // -------------------------------------------------------
     // Gestion des erreurs communes
     if (error.response?.status === 401) {
